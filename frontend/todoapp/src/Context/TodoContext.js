9a5cae4152d5b7e56a371105c7ca85c9193b8385
komal-tiwari-dev <atulzv1999@gmail.com>
2022-11-05 18:24:32 +0530

    Completed Todo app v3

--- frontend/todoapp/src/Context/TodoContext.js
+++ frontend/todoapp/src/Context/TodoContext.js
@@ -5,24 +5,29 @@ export const TodoContext=createContext()
 export const TodoProvider=({children})=>{
     const [todo,setTodo]=useState([])
     const getTodo=()=>{
-        axios.get("http://localhost:8000/getalltodo")
-        .then((res)=>{setTodo(res.data)
-        console.log("GetTodo",res.data)
-        })
+        axios
+          .get("https://warm-tor-91169.herokuapp.com/getalltodo")
+          .then((res) => {
+            setTodo(res.data);
+            console.log("GetTodo", res.data);
+          });
         console.log("InnerTodo",todo)
     }
     const addTodo=(data)=>{
-        axios.post("http://localhost:8000/addtodo",data)
-        .then((res)=>getTodo())
+        axios
+          .post("https://warm-tor-91169.herokuapp.com/addtodo", data)
+          .then((res) => getTodo());
     }
     const updateTodo=(data,id)=>{
         console.log("IDD",id)
-        axios.patch(`http://localhost:8000/updatetodo/${id}`,data)
-        .then((res)=>getTodo())
+        axios
+          .patch(`https://warm-tor-91169.herokuapp.com/updatetodo/${id}`, data)
+          .then((res) => getTodo());
     }
     const deleteTodo=(id)=>{
         axios
-          .delete(`http://localhost:8000/deletetodo/${id}`).then(res=>getTodo())
+          .delete(`https://warm-tor-91169.herokuapp.com/deletetodo/${id}`)
+          .then((res) => getTodo());
     }
     return (
       <TodoContext.Provider value={{ todo,getTodo,addTodo,updateTodo,deleteTodo }}>
